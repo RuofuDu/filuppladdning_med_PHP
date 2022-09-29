c75bf6bb35ba6c030a791106731b37ec59a58b15
Ruofu Du <ruofu.du@skola.taby.se>
2022-09-29 13:34:25 +0200

    chgane

--- laddaupp.php
+++ laddaupp.php
@@ -1,3 +1,17 @@
 <?php
 session_start();
-echo $_SESSION["anvandernamn"];
+$fil = $_POST["fil"];
+$anvandernamn = $_SESSION["anvandernamn"];
+
+
+if ($fil && $anvandernamn) {
+    $textfil = fopen('textfil.txt', 'a');
+    fwrite($textfil, "$anvandernamn:$fil <br>");
+    fclose($textfil);
+    $textfil = fopen('textfil.txt', 'r');
+    $readfile = fread($textfil, filesize('textfil.txt'));
+    echo $readfile;
+    fclose($textfil);
+} else {
+    echo "fil saknas.";
+}
--- welcome.php
+++ welcome.php
@@ -1,15 +1,33 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="en">
 
-$anvandernamn = $_POST["användarnamn"];
-$password = $_POST["losenord"];
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
 
-$_SESSION["anvandernamn"] = $anvandernamn;
+<body>
+    <?php
+    session_start();
 
-if ($anvandernamn && $password) {
-    echo "Hej $anvandernamn, du är inloggad. :)";
-} else {
-    echo "Tyvärr, det gick inte att logga in. :(";
-}
+    $anvandernamn = $_POST["användarnamn"];
+    $password = $_POST["losenord"];
 
-anteckningar
+    $_SESSION["anvandernamn"] = $anvandernamn;
+
+    if ($anvandernamn && $password) {
+        echo "Välkommen! Du är inloggad :) <br> <br>";
+        echo "<form action='laddaupp.php' method='post'>
+        <input type='file' name='fil' id='fil'>
+        <input type='submit'>
+        </form>";
+    } else {
+        echo "Tyvärr, det gick inte att logga in. :( <br> <br>";
+    }
+    ?>
+
+</body>
+
+</html>
