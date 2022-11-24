eaf60d98b44e96035cd28bb90f2408d3b9bb5955
Ruofu Du <ruofu.du@skola.taby.se>
2022-11-24 12:55:34 +0100

    change

--- laddaupp.php
+++ laddaupp.php
@@ -1,4 +1,17 @@
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "webbserverprogrammering";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully <br>";
+
 session_start();
 $fil = $_POST["fil"];
 $anvandernamn = $_SESSION["anvandernamn"];
@@ -12,7 +25,16 @@ if ($fil && $anvandernamn) {
     $readfile = fread($textfil, filesize('textfil.txt'));
     echo $readfile;
     fclose($textfil);
-    echo "<br><a href='http://localhost/filuppladdning_med_php/filuppladdning_med_PHP.html'>Tillbaka till inloggningen</a>";
 } else {
     echo "fil saknas.";
 }
+if ($anvandernamn == "holros") {
+    $sql = "INSERT INTO uploads (filename, user, uploadtime, snuskig)
+	VALUES ('$fil', '" . $anvandernamn . "', NOW(), TRUE)";
+    $conn->query($sql);
+} else {
+    $sql = "INSERT INTO uploads (filename, user, uploadtime, snuskig) VALUES ('$fil', '" . $anvandernamn . "', NOW(), TRUE)";
+    $result = $conn->query($sql);
+}
+
+echo "<br><a href='http://localhost/filuppladdning_med_php/filuppladdning_med_PHP.html'>Tillbaka till inloggningen</a>";
